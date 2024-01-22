fa268bb1eabc3a2f4e40aac35bc05dfa4fe954b0
Angel Sanchez <asanchezg1507@alumno.ipn.mx>
2024-01-22 15:20:43 -0600

    Update index.js
    Se agregan headers para CORS

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const { usersController } = require("./src/controller/users.controller");
 
 exports.handler = async (event) => {
   const headers = {
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, DELETE",
     "Content-Type": "application/json",
   };
+
+  if (event.requestContext.http.method == "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers,
+    };
+  }
 
   const result = await usersController(event);
 
   return {
     statusCode: result.statusCode,
-    body: result.body,
     headers,
+    body: result.body,
   };
 };
